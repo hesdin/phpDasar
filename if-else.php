d30a89660d527ba9ffc11be0791039c96242bc70
hesdin <hesdin01@gmail.com>
2022-11-28 16:21:08 +0800

    if else

---
+++ if-else.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>If Else</title>
+</head>
+
+<body>
+
+  <?php
+  $kelas = 'a10';
+
+  if ($kelas == 'a1') {
+    echo 'Hai Kelas A1';
+  } else if ($kelas == 'a2') {
+    echo 'Hai Kelas A2';
+  } else if ($kelas == 'a3') {
+    echo 'Hai Kelas A3';
+  } else {
+    echo 'kelas tidak ada';
+  }
+  ?>
+</body>
+
+</html>
